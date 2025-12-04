87a6d6dbd82cd9a5494f287c1efdb6999dcad134
Adrià Lorente <adria810@gmail.com>
2025-12-04 11:34:09 +0100

    feat: initialize Spring Boot project

---
+++ src/main/java/com/qiromanager/qiromanager_backend/QiromanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qiromanager.qiromanager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiromanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiromanagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qiromanager/qiromanager_backend/QiromanagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiromanager.qiromanager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiromanagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
