bce122368a9b96b7f4007af9010a26743f7a8208
Adrià Lorente <adria810@gmail.com>
2025-12-10 10:19:02 +0100

    feat(security): add GrantedAuthority support to Role enum

--- src/main/java/com/qiromanager/qiromanager_backend/domain/user/Role.java
+++ src/main/java/com/qiromanager/qiromanager_backend/domain/user/Role.java
@@ -1,6 +1,14 @@
 package com.qiromanager.qiromanager_backend.domain.user;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
 public enum Role {
     ADMIN,
-    USER
+    USER;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    }
 }
