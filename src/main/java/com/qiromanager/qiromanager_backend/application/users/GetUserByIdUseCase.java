272a0a35dd7eb2ba3d07f627e987cda99e202c42
Adrià Lorente <adria810@gmail.com>
2025-12-16 12:52:20 +0100

    refactor: decouple List and Get use cases from API DTOs

--- src/main/java/com/qiromanager/qiromanager_backend/application/users/GetUserByIdUseCase.java
+++ src/main/java/com/qiromanager/qiromanager_backend/application/users/GetUserByIdUseCase.java
@@ -1,7 +1,5 @@
 package com.qiromanager.qiromanager_backend.application.users;
 
-import com.qiromanager.qiromanager_backend.api.mappers.UserMapper;
-import com.qiromanager.qiromanager_backend.api.users.UserResponse;
 import com.qiromanager.qiromanager_backend.domain.exceptions.UserNotFoundException;
 import com.qiromanager.qiromanager_backend.domain.user.User;
 import com.qiromanager.qiromanager_backend.domain.user.UserRepository;
@@ -14,10 +12,8 @@ public class GetUserByIdUseCase {
 
     private final UserRepository userRepository;
 
-    public UserResponse execute(Long id) {
-        User user = userRepository.findById(id)
+    public User execute(Long id) {
+        return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
-
-        return UserMapper.toResponse(user);
     }
 }
--- src/main/java/com/qiromanager/qiromanager_backend/application/users/ListUsersUseCase.java
+++ src/main/java/com/qiromanager/qiromanager_backend/application/users/ListUsersUseCase.java
@@ -1,7 +1,6 @@
 package com.qiromanager.qiromanager_backend.application.users;
 
-import com.qiromanager.qiromanager_backend.api.mappers.UserMapper;
-import com.qiromanager.qiromanager_backend.api.users.UserResponse;
+import com.qiromanager.qiromanager_backend.domain.user.User;
 import com.qiromanager.qiromanager_backend.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,10 +13,7 @@ public class ListUsersUseCase {
 
     private final UserRepository userRepository;
 
-    public List<UserResponse> execute() {
-        return userRepository.findAll()
-                .stream()
-                .map(UserMapper::toResponse)
-                .toList();
+    public List<User> execute() {
+        return userRepository.findAll();
     }
 }
